ee83650a149ecf471cd646199e56df9002276ee0
yuribrito01 <94576514+yuribrito01@users.noreply.github.com>
2023-01-13 23:07:24 -0300

    Realizando algumas alterações no codigo

--- listaLivros.js
+++ listaLivros.js
@@ -6,19 +6,23 @@ const precosLivros = [
     {
         titulo: "PHP",
         preco: 15
-    },  {
+    },  
+    {
         titulo: "Java",
         preco: 30
-    },  {
+    },  
+    {
         titulo: "Elixir",
         preco: 50
-    },  {
+    },  
+    {
         titulo: "Go",
         preco: 45
-    },  {
+    },  
+    {
         titulo: "Python",
         preco: 20
     }
 ];
 
-// module.exports = primeiroCodigo;
+module.exports = precosLivros;
